657518bb61338b471178bef69bfd462a9081dab5
unknown <theharoldroyce@yahoo.com>
2022-07-20 21:23:36 +0800

    new update

---
+++ src/component/Workitems.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./workitems.css";
+
+function Workitems() {
+  return (
+    <div className="wi">Card</div>
+  )
+}
+export default Workitems;
--- src/component/Works.js
+++ src/component/Works.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import Workitems from './Workitems';
 import "./works.css";
 
  function Works() {
   return (
-    <div>Works</div>
+    <div className="w">
+        <div className="w-text">
+            <h1 className="w-title"> My Works and Projects </h1>
+            <p className="w-desc">
+                lorem am  here to build and Design exceptional Web Software and Application
+            </p>
+        </div>
+        <div className="w-pl">
+                <Workitems />
+                <Workitems />
+                <Workitems />
+                <Workitems />
+                <Workitems />
+                <Workitems />
+            </div>
+      
+    </div>
   )
 }
 export default Works;
